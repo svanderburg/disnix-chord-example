aa37cae3feb70db86136b5f59d8e5241d327c9ea
Sander van der Burg <svanderburg@gmail.com>
2020-09-28 22:53:44 +0200

    Migrate to the python test driver

--- release.nix
+++ release.nix
@@ -7,19 +7,18 @@
 let
   pkgs = import nixpkgs {};
 
+  disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+    inherit nixpkgs;
+  };
+
+  version = builtins.readFile ./version;
+
   jobs = rec {
-    tarball =
-      let
-        disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
-          inherit nixpkgs;
-        };
-      in
-      disnixos.sourceTarball {
-        name = "disnix-chord-example-tarball";
-        version = builtins.readFile ./version;
-        src = disnix_chord_example;
-        inherit officialRelease;
-      };
+    tarball = disnixos.sourceTarball {
+      name = "disnix-chord-example-tarball";
+      src = disnix_chord_example;
+      inherit officialRelease version;
+    };
 
     builds = pkgs.lib.genAttrs systems (system:
       let
@@ -29,8 +28,7 @@ let
       in
       disnixos.buildManifest {
         name = "disnix-chord-example";
-        version = builtins.readFile ./version;
-        inherit tarball;
+        inherit tarball version;
         servicesFile = "deployment/DistributedDeployment/services.nix";
         networkFile = "deployment/DistributedDeployment/network.nix";
         distributionFile = "deployment/DistributedDeployment/distribution.nix";
@@ -38,10 +36,6 @@ let
 
     tests =
       let
-        disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
-          inherit nixpkgs;
-        };
-
         customPkgs = import ./deployment/top-level/all-packages.nix {
           pkgs = import nixpkgs {};
         };
@@ -54,10 +48,12 @@ let
         inherit tarball manifest;
         networkFile = "deployment/DistributedDeployment/network.nix";
         testScript = ''
-          $test1->mustSucceed("sleep 10");
-          $test1->mustSucceed("((echo 'joinN -port 9000 -bootstrap test2:8001'; sleep 10; echo 'refsN'; echo 'exit'; echo 'y') | ${openchord}/bin/openchord-console > out) &");
-          $test1->mustSucceed("sleep 30");
-          $test1->mustSucceed("[ \$(grep -c 'ocsocket://test' out) -eq 6 ]");
+          test1.succeed("sleep 10")
+          test1.succeed(
+              "((echo 'joinN -port 9000 -bootstrap test2:8001'; sleep 10; echo 'refsN'; echo 'exit'; echo 'y') | ${openchord}/bin/openchord-console > out) &"
+          )
+          test1.succeed("sleep 30")
+          test1.succeed("[ $(grep -c 'ocsocket://test' out) -eq 6 ]")
         '';
       };
   };
